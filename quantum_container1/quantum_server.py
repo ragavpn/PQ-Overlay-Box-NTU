af9cb23f6534c206faa59e40a4232366ec68c9bd
ragavpn <106122095@nitt.edu>
2024-02-01 07:53:07 +0530

    OQS connection enabled

--- quantum_container1/quantum_server.py
+++ quantum_container1/quantum_server.py
@@ -1,8 +1,7 @@
-# quantum_server.py
-
 import socket
+import oqs
+import pickle
 
-# Setup socket for receiving data from Machine A
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 12345))  # Bind to port 12345
 s.listen(1)
@@ -21,6 +20,23 @@ conn_a.close()
 # Setup socket for sending data to Quantum Container 2
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('quantum_container2', 12346))  # Connect to Quantum Container 2
-client_socket.send(data)  # Send data to Quantum Container 2
-print(f"Data sent to Quantum Container 2: {data.decode()}")
+
+# Create a Key Encapsulation object
+kemalg = "Kyber512"
+with oqs.KeyEncapsulation(kemalg) as server:
+    # Receive the public key from Quantum Container 2
+    public_key_client = client_socket.recv(1024)
+
+    # Encapsulate the data
+    ciphertext, shared_secret_server = server.encap_secret(public_key_client)
+
+    # Create a list of values
+    values = [ciphertext, shared_secret_server, data]
+
+    # Serialize the list into a byte stream
+    serialized_values = pickle.dumps(values)
+
+    # Send the ciphertext to Quantum Container 2
+    client_socket.send(serialized_values)
+    print(f"Ciphertext sent to Quantum Container 2: {data.decode()}")
 client_socket.close()
--- quantum_container2/quantum_client.py
+++ quantum_container2/quantum_client.py
@@ -1,5 +1,6 @@
-# quantum_client.py
 import socket
+import oqs
+import pickle
 
 # Setup socket for receiving data from Quantum Container 1
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,16 +11,40 @@ s.listen(1)
 conn_q, addr_q = s.accept()
 print(f"Connection from Quantum Container 1: {addr_q}")
 
-# Receive data from Quantum Container 1
-data = conn_q.recv(1024)
-print(f"Received data from Quantum Container 1: {data.decode()}")
+# Create a Key Encapsulation object
+kemalg = "Kyber512"
+with oqs.KeyEncapsulation(kemalg) as client:
+    # Generate a key pair
+    public_key_client = client.generate_keypair()
 
-# Close connection with Quantum Container 1
-conn_q.close()
+    # Send the public key to Quantum Container 1
+    conn_q.send(public_key_client)
+    print("Public key sent to Quantum Container 1")
+
+    # Receive the serialized list from the socket
+    serialized_values = conn_q.recv(1024)
+
+    # Deserialize the list
+    values = pickle.loads(serialized_values)
+    ciphertext = values[0]
+    shared_secret_server = values[1]
+    data = values[2]
+
+    # the client decapsulates the server's ciphertext to obtain the shared secret
+    shared_secret_client = client.decap_secret(ciphertext)
+
+    if (shared_secret_client != shared_secret_server):
+        raise Exception("Shared secrets are not equal!")
+    else:
+        print("Shared secrets are equal!")
+    
+    print(f"Data received from Quantum Container 1: {data.decode()}")
+    
+    conn_q.close()
 
 # Setup socket for sending data to Machine B
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('machine_b', 12347))  # Connect to Machine B using its IP address
 client_socket.send(data)  # Send data to Machine B
-print(f"Data sent to Machine B: {data.decode()}")
+print(f"Data sent to Machine B: {data}")
 client_socket.close()
